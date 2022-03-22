4e3ec0394c5df8f8aecfc11e5e9497f347f682e1
wanapito <ivannr20@gmail.com>
2022-03-22 14:26:47 +0100

    1

--- PildorasInformaticas/src/poo/pruebas.java
+++ PildorasInformaticas/src/poo/pruebas.java
@@ -4,17 +4,21 @@ public class pruebas {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-		
-		//creamos objetos de la clase Empleados 
-		
-		Empleados trabajador1=new Empleados("Paco");
-		Empleados trabajador2 =new Empleados("ana");
-		
-		//vamos a cambiar seccion atraves del setter 
+
+		// creamos objetos de la clase Empleados
+
+		Empleados trabajador1 = new Empleados("Paco");
+		Empleados trabajador2 = new Empleados("ana");
+		Empleados trabajador3 = new Empleados("antonio");
+		Empleados trabajador4 = new Empleados("maria");
+		// vamos a cambiar seccion atraves del setter
 		trabajador1.cambiaSeccion("RRHH");
-		//vamos a sacar los datos con el getter
+		trabajador1.cambiaNombre("maria");
+		// vamos a sacar los datos con el getter
 		System.out.println(trabajador1.devuleveDatos());
 		System.out.println(trabajador2.devuleveDatos());
+		System.out.println(trabajador3.devuleveDatos());
+		System.out.println(trabajador4.devuleveDatos());
 
 	}
 
@@ -22,24 +26,38 @@ public class pruebas {
 
 //creamos una clase 
 class Empleados {
-	//creamos el contructor 
-	public Empleados (String nom) {
-		nombre =nom;
-		seccion ="administracion";
-		
+	// creamos el contructor
+	public Empleados(String nom) {
+
+		nombre = nom;
+		seccion = "administracion";
+		// de esta manera vamos a incrementar el valor de ID
+		Id = IdSiguiente;
+		IdSiguiente++;
+
 	}
-	//contruimos shetter 
-	public void cambiaSeccion (String seccion) {
-		this.seccion=seccion;
+
+	// construimos shetter
+	public void cambiaSeccion(String seccion) {
+		this.seccion = seccion;
 	}
-	//contruimos getter
-	public String devuleveDatos(){
-		return "el nombre es: "+  nombre + " y la seccion es "+ seccion;
+
+	// construimos un setter
+	public void cambiaNombre(String nombre) {
+		this.nombre = nombre;
 	}
-	//cremaos los atributos
+
+	// construimos getter
+	public String devuleveDatos() {
+		return "el nombre es: " + nombre + " la seccion es " + seccion + "y el id es =" + Id;
+	}
+
+	// cremaos los atributos
+	// con "final" no podemos modificar la varibale
 	private String nombre;
+	// private final String nombre;
 	private String seccion;
-	
-	
-	
+	private int Id;
+	private static int IdSiguiente = 1;
+
 }

88fccb373a952e6ddb11330e994617c221f55ab9
wanapito <ivannr20@gmail.com>
2022-04-20 10:31:42 +0200

    1

--- Udemy/src/dominio_palabraFinal/empleado.java
+++ Udemy/src/dominio_palabraFinal/empleado.java
@@ -1,11 +1,9 @@
 package dominio_palabraFinal;
 
-public class empleado //extends persona  
-	//Al se una método final de la clase persona no de puede modificar 
-	//@Override
-	//Public void imprimir () {
-	//	System.out.println("Método imprimir desde clase hija ");
+public class empleado // Extends persona
+// Al se una método final de la clase persona no de puede modificar
+// @Override
+// Public void imprimir () {
+// System.out.println("Método imprimir desde clase hija ");
 {
-	}
-
-
+}
--- Udemy/src/dominio_palabraFinal/persona.java
+++ Udemy/src/dominio_palabraFinal/persona.java
@@ -2,25 +2,28 @@ package dominio_palabraFinal;
 
 public final class persona {
 
-	//al se final static se le llamara constante que se indicara en mayusculas
-	public final static int  MI_CONSTANTE =1;
-	//Método de tipo final no se podrá modificar el método 
+	// Al se final static se le llamara constante que se indicara en mayusculas
+	public final static int MI_CONSTANTE = 1;
+	// Método de tipo final no se podrá modificar el método
 	private String nombre;
 	/**
-	 *Getter
+	 * Getter
+	 * 
 	 * @return el nombre
 	 */
 	public String getNombre() {
 		return nombre;
 	}
 	/**
-	 *Setter
-	 * @param nombre el nombre a establecer
+	 * Setter
+	 * 
+	 * @param nombre
+	 *            El nombre a establecer
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public final void imprimir () {
+	public final void imprimir() {
 		System.out.println("Método imprimir");
 	}
 

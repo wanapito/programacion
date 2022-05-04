dab6200ba6700968e1af21e45ef7eeb431b28778
wanapito <ivannr20@gmail.com>
2022-05-04 11:49:23 +0200

    1

---
+++ Udemy3/src/Test/BloqueInicializacion/TestBloqueInicializacion.java
@@ -0,0 +1,17 @@
+package Test.BloqueInicializacion;
+
+import domino.BloqueInicializacion.Persona;
+
+public class TestBloqueInicializacion {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		Persona persona1 = new Persona();
+		System.out.println("persona1 :" + persona1);
+		System.out.println("");
+		Persona persona2 = new Persona();
+		System.out.println("persona2 :" + persona2);
+
+	}
+
+}
